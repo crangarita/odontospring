e8eb084ef5459211dde7385cc6580c5a125ec2ef
Carlos Rene Angarita Sanguino <crangarita@gmail.com>
2021-12-06 10:17:47 -0500

    Inicio del proyecto OdontoSpring

---
+++ src/main/java/co/odontospring/OdontospringApplication.java
@@ -0,0 +1,13 @@
+package co.odontospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/odontospring/OdontospringApplicationTests.java
@@ -0,0 +1,13 @@
+package co.odontospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
